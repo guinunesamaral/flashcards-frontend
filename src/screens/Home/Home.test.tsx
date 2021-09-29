adbf7c5842964e758b5202ebe6b40af07bfdf298
guilherme nunes <guinucode@protonmail.com>
2021-09-28 21:20:50 -0300

    Add store

--- src/screens/Home/Home.test.tsx
+++
@@ -1,8 +0,0 @@
-import renderer from "react-test-renderer";
-import Home from "./Home";
-
-test("Random number generator", () => {
-  const home = renderer.create(<Home></Home>);
-  let tree = home.toJson();
-  expect(tree).toMatchSnapshot();
-});
