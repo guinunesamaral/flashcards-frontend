8cc3a6c7d45e84540d354fb0ff773490e3b7855a
guilherme nunes <guinucode@protonmail.com>
2021-09-28 21:21:34 -0300

    Add shared files

---
+++ src/shared/auth.database.ts
@@ -0,0 +1,6 @@
+export const authDatabase = {
+  users: [
+    { email: "gui@email.com", password: "123456" },
+    { email: "exam@email.com", password: "123456" },
+  ],
+};
---
+++ src/shared/flashcard.database.ts
@@ -0,0 +1,107 @@
+export const flashcardDatabase = [
+  {
+    id: "1",
+    front:
+      "According to our present theory of solar system formation, how did Earth end up with enough water to make oceans?",
+    back: "The water was brought to the forming Earth by planetesimals that accreted beyond the orbit of Mars.",
+  },
+  {
+    id: "2",
+    front: "The 'giant impact hypothesis' refers to the idea that ",
+    back: "our Moon formed when a Mars-size object collided with the young Earth. ",
+  },
+  {
+    id: "3",
+    front:
+      "What do we mean when we say that a nucleus undergoes radioactive decay?",
+    back: "-The number of protons or neutrons (or both) in the nucleus changes",
+  },
+  {
+    id: "4",
+    front:
+      "According to present understanding, which of the following statements about the solar wind is not true?",
+    back: "It is even stronger today than it was when the Sun was young. It was much stronger when the Sun was young. ",
+  },
+  {
+    id: "5",
+    front:
+      "Which of the following best explains why we can rule out the idea that planets are usually formed by near-collisions between stars?",
+    back: "Stellar near-collisions are far too rare to explain all the planets now known to orbit nearby stars. ",
+  },
+  {
+    id: "6",
+    front:
+      "The nebular theory also predicts that the cloud will flatten into a disk as it shrinks in size. Which of the following best explains why the collapsing cloud should form a disk? ",
+    back: "Colliding cloud particles exchange angular momentum and, on average, end up with the rotation pattern for the cloud as a whole.",
+  },
+  {
+    id: "7",
+    front:
+      "According to our present theory of solar system formation, how did Earth end up with enough water to make oceans?",
+    back: "The water was brought to the forming Earth by planetesimals that accreted beyond the orbit of Mars.",
+  },
+  {
+    id: "8",
+    front: "The 'giant impact hypothesis' refers to the idea that ",
+    back: "our Moon formed when a Mars-size object collided with the young Earth. ",
+  },
+  {
+    id: "9",
+    front:
+      "What do we mean when we say that a nucleus undergoes radioactive decay?",
+    back: "-The number of protons or neutrons (or both) in the nucleus changes",
+  },
+  {
+    id: "10",
+    front:
+      "According to present understanding, which of the following statements about the solar wind is not true?",
+    back: "It is even stronger today than it was when the Sun was young. It was much stronger when the Sun was young. ",
+  },
+  {
+    id: "11",
+    front:
+      "Which of the following best explains why we can rule out the idea that planets are usually formed by near-collisions between stars?",
+    back: "Stellar near-collisions are far too rare to explain all the planets now known to orbit nearby stars. ",
+  },
+  {
+    id: "12",
+    front:
+      "The nebular theory also predicts that the cloud will flatten into a disk as it shrinks in size. Which of the following best explains why the collapsing cloud should form a disk? ",
+    back: "Colliding cloud particles exchange angular momentum and, on average, end up with the rotation pattern for the cloud as a whole.",
+  },
+  {
+    id: "13",
+    front:
+      "According to our present theory of solar system formation, how did Earth end up with enough water to make oceans?",
+    back: "The water was brought to the forming Earth by planetesimals that accreted beyond the orbit of Mars.",
+  },
+  {
+    id: "14",
+    front: "The 'giant impact hypothesis' refers to the idea that ",
+    back: "our Moon formed when a Mars-size object collided with the young Earth. ",
+  },
+  {
+    id: "15",
+    front:
+      "What do we mean when we say that a nucleus undergoes radioactive decay?",
+    back: "-The number of protons or neutrons (or both) in the nucleus changes",
+  },
+  {
+    id: "16",
+    front:
+      "According to present understanding, which of the following statements about the solar wind is not true?",
+    back: "It is even stronger today than it was when the Sun was young. It was much stronger when the Sun was young. ",
+  },
+  {
+    id: "17",
+    front:
+      "Which of the following best explains why we can rule out the idea that planets are usually formed by near-collisions between stars?",
+    back: "Stellar near-collisions are far too rare to explain all the planets now known to orbit nearby stars. ",
+  },
+  {
+    id: "18",
+    front:
+      "The nebular theory also predicts that the cloud will flatten into a disk as it shrinks in size. Which of the following best explains why the collapsing cloud should form a disk? ",
+    back: "Colliding cloud particles exchange angular momentum and, on average, end up with the rotation pattern for the cloud as a whole.",
+  },
+];
